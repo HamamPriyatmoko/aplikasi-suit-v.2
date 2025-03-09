38be7a3b368f954a484820ebcd177a0ffc6274f1
HamamPriyatmoko <hamampriyatmoko81@gmail.com>
2025-03-09 15:47:54 +0700

    membuat function gethasil dan computer pilih

--- script.js
+++ script.js
@@ -1,38 +1,33 @@
-var tanya = true;
-while( tanya ) {
-    // menangkap pilihan player
-    var p = prompt('pilih : gajah, semut, orang');
+function computerPilih() {
+  const comp = Math.random();
 
-    // menangkap pilihan computer
-    // membangkitkan bilangan random
-    var comp = Math.random();
-
-    if( comp < 0.34 ) {
-        comp = 'gajah';
-    } else if( comp >= 0.34 && comp < 0.67 ) {
-        comp = 'orang';
-    } else {
-        comp = 'semut';
-    }
-
-    // menentukan rules
-    var hasil = '';
-    if( p == comp ) {
-        hasil = 'SERI!';
-    } else if( p == 'gajah' ) {
-        hasil = ( comp == 'orang' ) ? 'MENANG!' : 'KALAH!';
-    } else if( p == 'orang' ) {
-        hasil = ( comp == 'gajah' ) ? 'KALAH!' : 'MENANG!';
-    } else if( p == 'semut' ) {
-        hasil = ( comp == 'orang' ) ? 'KALAH' : 'MENANG!';
-    } else {
-        hasil = 'memasukkan pilihan yang salah!';
-    }
-
-    // tampilkan hasilnya
-    alert('Kamu memilih : ' + p + ' dan Komputer memilih : ' + comp + '\nMaka hasilnya : Kamu ' + hasil);
-
-    tanya = confirm('lagi?');
+  if (comp < 0.34) return 'gajah';
+  if (comp >= 0.34 && comp < 0.67) return 'orang';
+  return 'semut';
 }
 
-alert('terimakasih sudah bermain.');
+function getHasil(comp, player) {
+  if (player == comp) return 'SERI!';
+  if (player == 'gajah') return comp == 'orang' ? 'MENANG!' : 'KALAH!';
+  if (player == 'orang') return comp == 'gajah' ? 'KALAH!' : 'MENANG!';
+  if (player == 'semut') return comp == 'orang' ? 'KALAH' : 'MENANG!';
+}
+
+// var tanya = true;
+// while( tanya ) {
+//     // menangkap pilihan player
+//     var p = prompt('pilih : gajah, semut, orang');
+
+//     // menangkap pilihan computer
+//     // membangkitkan bilangan random
+
+//     // menentukan rules
+//
+
+//     // tampilkan hasilnya
+//     alert('Kamu memilih : ' + p + ' dan Komputer memilih : ' + comp + '\nMaka hasilnya : Kamu ' + hasil);
+
+//     tanya = confirm('lagi?');
+// }
+
+// alert('terimakasih sudah bermain.');
